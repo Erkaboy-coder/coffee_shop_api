5ede0af509340b72bc617729df7a857ccaac751c
Erkaboy-coder <erkaboy756.5676@gmail.com>
2025-10-24 16:20:36 +0500

    new

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import User
+from users.models import User
 
-@admin.register(User)
+# @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("id", "email", "role", "is_verified", "is_staff", "date_joined")
-    search_fields = ("email",)
+    search_fields = ("email", )
     list_filter = ("role", "is_verified", "is_staff")
+
+admin.site.register(User, UserAdmin)
